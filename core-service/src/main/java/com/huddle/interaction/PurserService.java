940e71036008c408c48bba17b54cb769c0cd8657
Yash Bhatnagar <ybhatnagar@vmware.com>
2019-10-18 00:24:04 +0530

    updated the few contracts

--- core-service/src/main/java/com/huddle/interaction/PurserService.java
+++ core-service/src/main/java/com/huddle/interaction/PurserService.java
@@ -1,7 +1,34 @@
 package com.huddle.interaction;
 
+import com.huddle.model.ClusterResponse;
+import com.huddle.model.PodResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class PurserService {
+
+    @Value("purser.base.url")
+    String PURSER_BASE_URL;
+
+    public ClusterResponse getClusterInfo(){
+
+
+        //TODO: actual call via restTemplate to the purser, to get actual info
+        ClusterResponse resp = new ClusterResponse();
+
+        ClusterResponse.ClusterInternal clusterInternal  = new ClusterResponse.ClusterInternal();
+
+        PodResponse podResponse = new PodResponse("pod1","node1","10.112.31.3","borathon");
+        PodResponse podResponse2 = new PodResponse("pod2","node2","10.112.31.5","borathon");
+
+        clusterInternal.setPods(Arrays.asList(podResponse,podResponse2));
+        clusterInternal.setClusterName("Staging-eng-services");
+
+
+        resp.setCluster(Arrays.asList(clusterInternal));
+        return resp;
+    }
 }
