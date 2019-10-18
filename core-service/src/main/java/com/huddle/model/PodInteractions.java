1df33463019097f01eed6414a8e5eba45f0c8f2d
Amit Kumar <amit20587@gmail.com>
2019-10-18 07:26:23 +0000

    Adding mock data

--- core-service/src/main/java/com/huddle/model/PodInteractions.java
+++ core-service/src/main/java/com/huddle/model/PodInteractions.java
@@ -8,8 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PodInteractions {
-    long interactionCount;
     String destinationName;
     String destinationIp;
+    long interactionCount;
     long latencyInMs;
 }
