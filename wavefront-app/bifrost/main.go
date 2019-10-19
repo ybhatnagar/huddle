ceca3da802458ff4158b27beff7e2fb8900674d0
palash <jpalash@vmware.com>
2019-10-19 20:01:29 +0530

    Description: 1.Bug fix in borathon-api. Missed encoding of query parameters 2.Added interactions in bifrost 3.Rempved vendor folder from borathon

--- wavefront-app/bifrost/main.go
+++ wavefront-app/bifrost/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -13,26 +13,114 @@ func main() {
 	for {
 
 	}
-
 }
 
 func pingApp() {
-	for i := 0; i < 1; i++ {
+	for {
+		time.Sleep(500 * time.Millisecond)
 		httpClient := &http.Client{}
-		httpReq, _ := http.NewRequest("GET", "http://google.com", nil)
+		httpReq, _ := http.NewRequest("GET", "http://cortex-0.cortex.default.svc.cluster.local:8080/borathonapi/testcrossgeo", nil)
 		q := httpReq.URL.Query()
-		q.Add("sourceDelay", "42")
-		q.Add("destinationDelay", "22")
-		q.Add("serviceUrl","")
-		q.Add("destinationUrl","")
+		q.Add("sourceDelay", "2")
+		q.Add("destinationDelay", "1")
+		q.Add("serviceUrl", "http://heimdall-0.heimdall.default.svc.cluster.local:8080/borathonapi/testcrossgeo")
+		q.Add("destinationUrl", "http://lemans-resources-0.lemans-resources.default.svc.cluster.local:8080/borathonapi/test")
 		httpReq.URL.RawQuery = q.Encode()
-
-		fmt.Println(httpReq.URL.String())
-
-		resp, err := httpClient.Do(httpReq)
+		_, err := httpClient.Do(httpReq)
 		if err != nil {
 			log.Println(err)
 		}
-		log.Println(resp)
+
+		httpClient = &http.Client{}
+		httpReq, _ = http.NewRequest("GET", "http://lint-app-0.lint-app.default.svc.cluster.local:8080/borathonapi/testcrossgeo", nil)
+		q = httpReq.URL.Query()
+		q.Add("sourceDelay", "22")
+		q.Add("destinationDelay", "5")
+		q.Add("serviceUrl", "http://heimdall-1.heimdall.default.svc.cluster.local:8080/borathonapi/testcrossgeo")
+		q.Add("destinationUrl", "http://stargate-deployment-0.stargate-deployment.default.svc.cluster.local:8080/borathonapi/test")
+		httpReq.URL.RawQuery = q.Encode()
+		_, err = httpClient.Do(httpReq)
+		if err != nil {
+			log.Println(err)
+		}
+
+		httpClient = &http.Client{}
+		httpReq, _ = http.NewRequest("GET", "http://cost-analysis-0.cost-analysis.default.svc.cluster.local:8080/borathonapi/testcrossgeo", nil)
+		q = httpReq.URL.Query()
+		q.Add("sourceDelay", "3")
+		q.Add("destinationDelay", "")
+		q.Add("serviceUrl", "http://heimdall-1.heimdall.default.svc.cluster.local:8080/borathonapi/test")
+		q.Add("destinationUrl", "")
+		httpReq.URL.RawQuery = q.Encode()
+		_, err = httpClient.Do(httpReq)
+		if err != nil {
+			log.Println(err)
+		}
+
+		httpClient = &http.Client{}
+		httpReq, _ = http.NewRequest("GET", "http://stargate-deployment-0.stargate-deployment.default.svc.cluster.local:8080/borathonapi/testcrossgeo", nil)
+		q = httpReq.URL.Query()
+		q.Add("sourceDelay", "20")
+		q.Add("destinationDelay", "")
+		q.Add("serviceUrl", "http://cortex-0.cortex.default.svc.cluster.local:8080/borathonapi/test")
+		q.Add("destinationUrl", "")
+		httpReq.URL.RawQuery = q.Encode()
+		_, err = httpClient.Do(httpReq)
+		if err != nil {
+			log.Println(err)
+		}
+
+		httpClient = &http.Client{}
+		httpReq, _ = http.NewRequest("GET", "http://lemans-gateway-0.lemans-gateway.default.svc.cluster.local:8080/borathonapi/testcrossgeo", nil)
+		q = httpReq.URL.Query()
+		q.Add("sourceDelay", "17")
+		q.Add("destinationDelay", "")
+		q.Add("serviceUrl", "http://lemans-resources-0.lemans-resources.default.svc.cluster.local:8080/borathonapi/test")
+		q.Add("destinationUrl", "")
+		httpReq.URL.RawQuery = q.Encode()
+		_, err = httpClient.Do(httpReq)
+		if err != nil {
+			log.Println(err)
+		}
+
+		httpClient = &http.Client{}
+		httpReq, _ = http.NewRequest("GET", "http://lint-app-0.lint-app.default.svc.cluster.local:8080/borathonapi/testcrossgeo", nil)
+		q = httpReq.URL.Query()
+		q.Add("sourceDelay", "2")
+		q.Add("destinationDelay", "")
+		q.Add("serviceUrl", "http://lint-ui-0.lint-ui.default.svc.cluster.local:8080/borathonapi/test")
+		q.Add("destinationUrl", "")
+		httpReq.URL.RawQuery = q.Encode()
+		_, err = httpClient.Do(httpReq)
+		if err != nil {
+			log.Println(err)
+		}
+
+		httpClient = &http.Client{}
+		httpReq, _ = http.NewRequest("GET", "http://lint-app-0.lint-app.default.svc.cluster.local:8080/borathonapi/testcrossgeo", nil)
+		q = httpReq.URL.Query()
+		q.Add("sourceDelay", "12")
+		q.Add("destinationDelay", "")
+		q.Add("serviceUrl", "http://lemans-gateway-0.lemans-gateway.default.svc.cluster.local:8080/borathonapi/test")
+		q.Add("destinationUrl", "")
+		httpReq.URL.RawQuery = q.Encode()
+		_, err = httpClient.Do(httpReq)
+		if err != nil {
+			log.Println(err)
+		}
+
+		httpClient = &http.Client{}
+		httpReq, _ = http.NewRequest("GET", "http://stargate-deployment-0.stargate-deployment.default.svc.cluster.local:8080/borathonapi/testcrossgeo", nil)
+		q = httpReq.URL.Query()
+		q.Add("sourceDelay", "42")
+		q.Add("destinationDelay", "")
+		q.Add("serviceUrl", "http://lemans-gateway-0.lemans-gateway.default.svc.cluster.local:8080/borathonapi/test")
+		q.Add("destinationUrl", "")
+		httpReq.URL.RawQuery = q.Encode()
+		_, err = httpClient.Do(httpReq)
+		if err != nil {
+			log.Println(err)
+		}
+
 	}
 }
